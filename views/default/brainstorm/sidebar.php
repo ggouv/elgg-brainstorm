5cc1b97714e08fc364c139b3fdd66edf6e475eac
ManUtopiK <emmanuel.salomon@free.fr>
2011-11-04 12:00:47 -0300

    fix sidebar

--- views/default/brainstorm/sidebar.php
+++ views/default/brainstorm/sidebar.php
@@ -2,14 +2,21 @@
 /**
  * Brainstorm group sidebar
  */
-echo elgg_view('brainstorm/sidebar-points');
+$user = elgg_get_logged_in_user_entity();
+$page_owner = elgg_get_page_owner_entity();
 
-echo elgg_view('page/elements/comments_block', array(
-	'subtypes' => 'idea',
-	'owner_guid' => elgg_get_page_owner_guid(),
-));
+if ($page_owner->isMember($user)) {
 
-echo elgg_view('page/elements/tagcloud_block', array(
-	'subtypes' => 'idea',
-	'owner_guid' => elgg_get_page_owner_guid(),
-));
+	echo elgg_view('brainstorm/sidebar-points');
+	
+	echo elgg_view('page/elements/comments_block', array(
+		'subtypes' => 'idea',
+		'owner_guid' => elgg_get_page_owner_guid(),
+	));
+	
+	echo elgg_view('page/elements/tagcloud_block', array(
+		'subtypes' => 'idea',
+		'owner_guid' => elgg_get_page_owner_guid(),
+	));
+
+}
