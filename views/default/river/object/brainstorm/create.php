00cb64fb93f3e53d76aabec4cf3538152c6446c5
ManUtopiK <emmanuel.salomon@free.fr>
2011-11-24 23:35:54 -0300

    fix little bug when create river item

--- views/default/river/object/brainstorm/create.php
+++ views/default/river/object/brainstorm/create.php
@@ -10,6 +10,5 @@ $excerpt = elgg_get_excerpt($object->description, '100');
 
 echo elgg_view('river/item', array(
 	'item' => $vars['item'],
-	'message' => $excerpt,
-	'attachments' => elgg_view('output/url', array('href' => $object->address)),
+	'message' => $excerpt
 ));
