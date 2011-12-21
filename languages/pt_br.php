6e0212db546f81886ad04cbf3d6124e79eaab4ef
ManUtopiK <emmanuel.salomon@free.fr>
2011-12-21 04:12:45 -0300

    adding portugese brasilian language

---
+++ languages/pt_br.php
@@ -0,0 +1,186 @@
+<?php
+
+/**
+ * Brainstorm Brazilian Portuguese language file
+ * Tranlated by Matheus Borges Lopes from — Sua Chance : http://suachance.lucrebem.com.br
+ */
+
+ 
+
+$portugues_brasileiro = array(
+
+ 
+
+/**
+
+* Menu items and titles
+
+*/
+
+ 
+
+'brainstorm:filter:top' => "Top",
+
+'brainstorm:filter:hot' => "Quente",
+
+'brainstorm:filter:new' => "Nova",
+
+'brainstorm:filter:accepted' => "Aceita",
+
+'brainstorm:filter:completed' => "Completa",
+
+ 
+
+'item:object:idea' => 'Idéia',
+
+'river:create:object:idea' => '%s enviou uma idéia %s',
+
+'river:comment:object:idea' => '%s comentou em uma idéia %s',
+
+'river:edit:object:idea' => '%s editou uma idéia %s',
+
+'brainstorm:river:annotate' => 'comentou nesta idéia',
+
+'brainstorm:river:item' => 'um ítem',
+
+ 
+
+'brainstorm' => "Brainstorm",
+
+'brainstorm:add' => "Adicionar idéia",
+
+'brainstorm:edit' => "Editar idéia",
+
+'brainstorm:new' => "Uma nova idéia",
+
+ 
+
+'brainstorm:owner' => "Brainstorm de %s",
+
+'brainstorm:idea:edit' => "Editar esta idéia",
+
+'brainstorm:idea:add' => "Adicionar uma idéia",
+
+ 
+
+'brainstorm:enablebrainstorm' => "Ativar Brainstorm.",
+
+'brainstorm:group' => 'Brainstorm da equipe',
+
+'brainstorm:same_group' => "Na mesma equipe:",
+
+ 
+
+/**
+
+* Content
+
+*/
+
+'brainstorm:yourvotes' => "Seus votos:",
+
+'brainstorm:vote' => "Votar:",
+
+ 
+
+'brainstorm:status' => "Status:",
+
+'brainstorm:state' => "Estado:",
+
+'brainstorm:status_info' => "Informações sobre o estado:",
+
+'brainstorm:open' => 'Aberta',
+
+'brainstorm:under review' => 'Sob revisão',
+
+'brainstorm:planned' => 'planejada',
+
+'brainstorm:started' => 'iniciada',
+
+'brainstorm:completed' => 'completa',
+
+'brainstorm:declined' => 'rejeitada',
+
+ 
+
+'brainstorm:none' => "Sem idéias.",
+
+'brainstorm:novoteleft' => "voto restante.",
+
+'brainstorm:onevoteleft' => "voto restante.",
+
+'brainstorm:votesleft' => "votos restantes.",
+
+ 
+
+'brainstorm:search' => "Pesquisar ou enviar uma idéia:",
+
+'brainstorm:charleft' => "caracteres restantes.",
+
+'brainstorm:search:find' => "Idéias encontradas. Votar ou ",
+
+'brainstorm:search:none' => "Nenhuma idéia encontrada. Pesquisar novamente ou ",
+
+'brainstorm:add' => "Enviar uma nova idéia",
+
+ 
+
+/**
+
+* Widget and bookmarklet
+
+*/
+
+'brainstorm:widget:title' => "Brainstorm.",
+
+'brainstorm:widget:description' => "Exibir últimas idéias.",
+
+'brainstorm:more' => "Mais idéias",
+
+'brainstorm:numbertodisplay' => "Número de idéias para exibir:",
+
+ 
+
+/**
+
+* Status messages
+
+*/
+
+'brainstorm:idea:rate:submitted' => "Idéia votada com sucesso.",
+
+'brainstorm:idea:save:success' => "Sua idéia foi salva com sucesso.",
+
+'brainstorm:idea:delete:success' => "Sua idéia foi apagada com sucesso.",
+
+'brainstorm:idea:delete:failed' => "Um erro ocorreu ao tentar apagar a idéia.",
+
+ 
+
+'brainstorm:idea:save:empty' => "Você precisa especificar o título e a descrição da idéia.",
+
+'brainstorm:idea:save:failed' => "Um erro ocorreu ao tentar salvar a idéia.",
+
+ 
+
+/**
+
+* Error messages
+
+*/
+
+'brainstorm:idea:rate:error:ajax' => "Erro de conexão com o servidor.",
+
+'brainstorm:unknown_idea' => "Idéia desconhecida.",
+
+'brainstorm:idea:rate:error:value' => "Erro do valor para avaliar esta idéia.",
+
+'brainstorm:idea:rate:error' => "Esta idéia não pôde ser votada por causa de um erro interno no servidor, por favor, tente novamente.",
+
+'brainstorm:idea:rate:error:underzero' => "Seus votos restantes não permitem que você avalie numa idéia.",
+
+);
+
+ 
+
+add_translation('pt_br', $portugues_brasileiro); 
