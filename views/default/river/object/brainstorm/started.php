39d558f87fb62e61b08a6fab9feb408b753e2e9d
ManUtopiK <emmanuel.salomon@free.fr>
2013-09-24 23:17:56 +0200

    Prevent error on rivers items on closed group

--- views/default/river/object/brainstorm/started.php
+++ views/default/river/object/brainstorm/started.php
@@ -9,34 +9,36 @@ $object = $vars['item']->getObjectEntity();
 $subject = $vars['item']->getSubjectEntity();
 $container = $object->getContainerEntity();
 
-$subject_link = elgg_view('output/url', array(
-	'href' => $subject->getURL(),
-	'text' => $subject->name,
-	'class' => 'elgg-river-subject',
-	'is_trusted' => true,
-));
+if ($object && $subject && $container) {
+	$subject_link = elgg_view('output/url', array(
+		'href' => $subject->getURL(),
+		'text' => $subject->name,
+		'class' => 'elgg-river-subject',
+		'is_trusted' => true,
+	));
 
-$object_link = elgg_view('output/url', array(
-	'href' => $object->getURL(),
-	'text' => $object->title,
-	'class' => 'elgg-river-object',
-	'is_trusted' => true,
-));
+	$object_link = elgg_view('output/url', array(
+		'href' => $object->getURL(),
+		'text' => $object->title,
+		'class' => 'elgg-river-object',
+		'is_trusted' => true,
+	));
 
-$group_link = elgg_view('output/url', array(
-	'href' => $container->getURL(),
-	'text' => $container->name,
-	'is_trusted' => true,
-));
-$group_string = elgg_echo('river:ingroup', array($group_link));
+	$group_link = elgg_view('output/url', array(
+		'href' => $container->getURL(),
+		'text' => $container->name,
+		'is_trusted' => true,
+	));
+	$group_string = elgg_echo('river:ingroup', array($group_link));
 
-$status_array = unserialize($container->brainstorm_status);
-$status_string = $status_array['started'] ? $status_array['started'] : elgg_echo('brainstorm:started');
+	$status_array = unserialize($container->brainstorm_status);
+	$status_string = $status_array['started'] ? $status_array['started'] : elgg_echo('brainstorm:started');
 
-$excerpt = "<span class=\"status started\">$status_string</span>&nbsp;";
-$excerpt .= strip_tags(elgg_get_excerpt($object->status_info, 140));
+	$excerpt = "<span class=\"status started\">$status_string</span>&nbsp;";
+	$excerpt .= strip_tags(elgg_get_excerpt($object->status_info, 140));
 
-echo elgg_view('river/item', array(
-	'item' => $vars['item'],
-	'message' => $excerpt
-));
+	echo elgg_view('river/item', array(
+		'item' => $vars['item'],
+		'message' => $excerpt
+	));
+}
--- views/default/river/object/brainstorm/under_review.php
+++ views/default/river/object/brainstorm/under_review.php
@@ -9,34 +9,36 @@ $object = $vars['item']->getObjectEntity();
 $subject = $vars['item']->getSubjectEntity();
 $container = $object->getContainerEntity();
 
-$subject_link = elgg_view('output/url', array(
-	'href' => $subject->getURL(),
-	'text' => $subject->name,
-	'class' => 'elgg-river-subject',
-	'is_trusted' => true,
-));
+if ($object && $subject && $container) {
+	$subject_link = elgg_view('output/url', array(
+		'href' => $subject->getURL(),
+		'text' => $subject->name,
+		'class' => 'elgg-river-subject',
+		'is_trusted' => true,
+	));
 
-$object_link = elgg_view('output/url', array(
-	'href' => $object->getURL(),
-	'text' => $object->title,
-	'class' => 'elgg-river-object',
-	'is_trusted' => true,
-));
+	$object_link = elgg_view('output/url', array(
+		'href' => $object->getURL(),
+		'text' => $object->title,
+		'class' => 'elgg-river-object',
+		'is_trusted' => true,
+	));
 
-$group_link = elgg_view('output/url', array(
-	'href' => $container->getURL(),
-	'text' => $container->name,
-	'is_trusted' => true,
-));
-$group_string = elgg_echo('river:ingroup', array($group_link));
+	$group_link = elgg_view('output/url', array(
+		'href' => $container->getURL(),
+		'text' => $container->name,
+		'is_trusted' => true,
+	));
+	$group_string = elgg_echo('river:ingroup', array($group_link));
 
-$status_array = unserialize($container->brainstorm_status);
-$status_string = $status_array['under_review'] ? $status_array['under_review'] : elgg_echo('brainstorm:under_review');
+	$status_array = unserialize($container->brainstorm_status);
+	$status_string = $status_array['under_review'] ? $status_array['under_review'] : elgg_echo('brainstorm:under_review');
 
-$excerpt = "<span class=\"status under_review\">$status_string</span>&nbsp;";
-$excerpt .= strip_tags(elgg_get_excerpt($object->status_info, 140));
+	$excerpt = "<span class=\"status under_review\">$status_string</span>&nbsp;";
+	$excerpt .= strip_tags(elgg_get_excerpt($object->status_info, 140));
 
-echo elgg_view('river/item', array(
-	'item' => $vars['item'],
-	'message' => $excerpt
-));
+	echo elgg_view('river/item', array(
+		'item' => $vars['item'],
+		'message' => $excerpt
+	));
+}
